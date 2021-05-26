e7f445139079ea9df69b67aab98a5b20bffcc0eb
l0r5 <lars-braitinger@web.de>
2021-05-26 14:51:39 +0200

    Adjust content type

--- src/main/java/ch/l0r5/TradingViewAPI/api/WebhookController.java
+++ src/main/java/ch/l0r5/TradingViewAPI/api/WebhookController.java
@@ -1,6 +1,7 @@
 package ch.l0r5.TradingViewAPI.api;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class WebhookController {
 
-    @PostMapping(path="/trigger")
+    @PostMapping(path = "/trigger", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<HttpStatus> triggerSignal(@RequestBody Map<String, Object> signal) {
         log.info("Received object: {}", signal);
 
