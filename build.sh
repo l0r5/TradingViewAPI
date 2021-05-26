38af6eeb5a72fab69a646087f3645ee0fe22586e
l0r5 <lars-braitinger@web.de>
2021-05-26 14:45:22 +0200

    Cleanup and finish CI/CD Pipeline

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env sh
-
  docker build -t l0r5/trading-view-api .
--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-docker run l0r5/trading-view-api:latest -p 8080:8080
+docker run -p 8080:8080 l0r5/trading-view-api:latest
